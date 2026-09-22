6027d45c4af48d27179bac58b0753d11a280ad83
PeterNguyen4 <peternguyen644@gmail.com>
2026-09-21 23:52:56 -0700

    feat: overlay counter asset

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import background from './assets/background.webp'
+import counter from './assets/counter.webp'
 
 function App() {
   return (
     <div
       style={{
+        position: 'relative',
         backgroundImage: `url(${background})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         flex: 1,
       }}
     >
-      ...
+      <img
+        src={counter}
+        alt=""
+        style={{
+          position: 'absolute',
+          bottom: '-20vh',
+          left: '-8vw',
+          width: '60%',
+          transform: 'rotate(-0.4deg)',
+        }}
+      />
     </div>
   )
 }
