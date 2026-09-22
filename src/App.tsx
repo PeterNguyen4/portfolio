b0722f78801f29df263e9f79bc2e7cebe46561dc
PeterNguyen4 <peternguyen644@gmail.com>
2026-09-21 22:47:16 -0700

    feat: add profile background

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import background from './assets/background.webp'
+
 function App() {
   return (
-    <div>
-      <h1>Portfolio</h1>
+    <div
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        flex: 1,
+      }}
+    >
+      ...
     </div>
   )
 }
